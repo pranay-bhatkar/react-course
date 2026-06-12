c02e9acd2dcb6af2d7e0383d52ea5e87fa132476
pranay-bhatkar <pranaybhatkar81@gmail.com>
2026-06-12 12:36:48 +0530

    info on state and hook

---
+++ 03_COUNTER_STATE/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const [countToSet, setCountToSet] = useState(0);
+
+  // const incrementHandler = (numVal) => {
+  //   // setCount(numVal + 1)  short hand notation
+  //   setCount((prevValue) => prevValue +numVal + 1 );
+  //   setCount((prevValue) => prevValue + 1 );
+  //   setCount((prevValue) => prevValue + 1 );
+  // };
+
+  return (
+    <>
+      <h1>Counter is {count}</h1>
+      <div className="card">Count is {count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)} style={{ margin: "0 5px" }}>
+          Increase
+        </button>
+        <button
+          onClick={() => setCount((count) => Math.max(count - 1, 0))}
+          style={{ margin: "0 5px" }}
+        >
+          Decrease
+        </button>
+        <button
+          onClick={() => setCount((count) => 0)}
+          style={{ margin: "0 5px" }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <div style={{ margin: "10px 0" }}>
+          <input
+            style={{
+              width: "100px",
+              border: "1px solid white",
+              margin: "0 5px",
+              padding: "0.6em 1.2em",
+            }}
+            value={countToSet}
+            onChange={(e) => setCountToSet(Number(e.target.value))}
+            type="text"
+          />
+          <button
+            style={{
+              margin: "0 5px",
+            }}
+            onClick={() => {
+              setCount(Number(countToSet));
+              setCountToSet(0);
+            }}
+          >
+            Set to {countToSet}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;

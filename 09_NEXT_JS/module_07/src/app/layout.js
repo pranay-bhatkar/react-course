cafa4647db3b269582a8216fd1a826be11259b68
pranay-bhatkar <pranaybhatkar81@gmail.com>
2026-06-26 12:54:34 +0530

    next/font automatically optimizes your fonts (including custom fonts) and removes external network requests for improved privacy and performance. No requests are sent to Google by the browser. Local font

--- 09_NEXT_JS/module_07/src/app/layout.js
+++ 09_NEXT_JS/module_07/src/app/layout.js
@@ -1,14 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({
+  
+  subset: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -20,7 +27,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased `}
+      className={`${inter.className} h-full antialiased `}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>

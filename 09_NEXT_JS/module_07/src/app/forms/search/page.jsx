d542d28b67f489dfa988be5d2f9d2dff0f8330bf
pranay-bhatkar <pranaybhatkar81@gmail.com>
2026-06-26 15:04:47 +0530

    Image component in the next js with remote image(set remotePatterns in next.config file , local image )

--- 09_NEXT_JS/module_07/src/app/forms/search/page.jsx
+++ 09_NEXT_JS/module_07/src/app/forms/search/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const SearchPage = async ({ searchParams }) => {
@@ -14,6 +15,22 @@ const SearchPage = async ({ searchParams }) => {
       <h1>Search Result</h1>
       <h2>{data.title}</h2>
       <p>{data.body}</p>
+
+      <div>
+        <p>Images </p>
+        <div className=" flex flex-row border gap-4 ">
+          <div>
+            <Image
+              src={
+                "https://images.pexels.com/photos/5589339/pexels-photo-5589339.jpeg"
+              }
+              alt="sport car"
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

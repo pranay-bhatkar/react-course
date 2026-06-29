53c4b99350b61b513939e9f06ae683f629a93fb8
pranay-bhatkar <pranaybhatkar81@gmail.com>
2026-06-29 20:30:40 +0530

    class on the orm and prisma
    basic set for the prisma

---
+++ 09_NEXT_JS/module_10/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+const HomePage = () => {
+  return (
+    <div className="flex min-h-screen justify-center p-2 border-2">
+      <div className="space-y-2">
+        <p className="text-2xl font-bold border-b-white border-y-2 p-1 bg-blue-950">
+          Class on ORM and Prisma
+        </p>
+        <div>
+          <p>Hi i am pranay</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
